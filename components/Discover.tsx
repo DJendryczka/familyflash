b26bfd3727cae99724bec187aed10555d64152ef
Dariusz Jendryczka <88456245+DJendryczka@users.noreply.github.com>
2022-10-26 05:50:19 +0200

    Add content to sidebar

---
+++ components/Discover.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { topics } from '../utils/constants'
+
+const Discover = () => {
+  return (
+    <div className=' xl:border-b-2 xl:border-gray-200 pb-6'>
+        <p className=' xl:border-gray-500 font-semibold m-3 mt-4 hidden xl:block'>
+            Popular Topics
+        </p>
+    </div>
+  )
+}
+
+export default Discover
