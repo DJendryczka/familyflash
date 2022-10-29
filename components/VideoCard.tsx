8e5bbe31dcf1be9aae66eca32b4a3f49bb8aacb8
Dariusz Jendryczka <88456245+DJendryczka@users.noreply.github.com>
2022-10-29 13:18:43 +0200

    Add style to videocard component

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect, useRef} from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi'
+import { BsPlay, BsFillPlayFill, BsFillPauseFill, } from 'react-icons/bs';
+import { GoVerified } from 'react-icons/go'
 import { Video } from '../types'
 import { NextPage } from 'next'
 
@@ -8,7 +13,36 @@ interface IProps {
 
 const VideoCard: NextPage<IProps> = ({ post }) => {
   return (
-    <div>VideoCard</div>
+    <div className=' flex flex-col border-b-2 border-x-gray-200 pb-6'>
+        <div className=' flex gap-3 p-2 font-semibold rounded'>
+            <div className=' md:w-16 md:h-16 w-10 h-10'>
+                <Link href='/'>
+                    <>
+                        <Image 
+                            width={62}
+                            height={62}
+                            className=' rounded-full'
+                            src={post.postedBy.image}
+                            alt='Profile Photo'
+                            layout='responsive'
+                        />
+                    </>
+                </Link>
+            </div>
+            <div>
+                <Link href='/'>
+                    <div>
+                        <p>{post.postedBy.userName}
+                        <GoVerified 
+                        className='text-blue-400 text-md'
+                        />
+                        </p>
+                        <p>{post.postedBy.userName}</p>
+                    </div>
+                </Link>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    domains: ['unsplash.com']
+  }
 }
 
 module.exports = nextConfig
