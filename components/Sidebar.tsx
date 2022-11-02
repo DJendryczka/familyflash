f7715136e6e0b388beeb96ada002d595b176a333
Dariusz Jendryczka <88456245+DJendryczka@users.noreply.github.com>
2022-11-02 18:41:05 +0100

    Upload functionality completed

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,24 +1,26 @@
-import React, { useState } from 'react'
-import { NextPage } from 'next'
-import { useRouter } from 'next/router'
-import Link from 'next/link'
-import GoogleLogin from 'react-google-login'
+import React, { useState } from 'react';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import GoogleLogin from 'react-google-login';
 import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
 import { ImCancelCircle } from 'react-icons/im';
-import Discover from './Discover'
-import SuggestedAccounts from './SuggestedAccounts'
-import Footer from './Footer'
+import Discover from './Discover';
+import SuggestedAccounts from './SuggestedAccounts';
+import Footer from './Footer';
 
 const Sidebar = () => {
-const [ showSidebar, setShowSidebar ] = useState(true)
-const userProfile = false
-const normalLink = ' flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#F51997]'
+  const [showSidebar, setShowSidebar] = useState(true);
+  const userProfile = false;
+  const normalLink =
+    ' flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#F51997]';
   return (
     <div>
-      <div className=' block xl:hidden m-2 ml-4 mt-2 text-xl'
-      onClick={()=> setShowSidebar((prev) => !prev)}
+      <div
+        className=' block xl:hidden m-2 ml-4 mt-2 text-xl'
+        onClick={() => setShowSidebar((prev) => !prev)}
       >
-        { showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
+        {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
       </div>
       {showSidebar && (
         <div className='xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3'>
@@ -28,36 +30,11 @@ const normalLink = ' flex items-center gap-3 hover:bg-primary p-3 justify-center
                 <div className=' text-2xl'>
                   <AiFillHome />
                 </div>
-                <span className=' text-xl hidden xl:block'>
-                  For You
-                </span>
+                <span className=' text-xl hidden xl:block'>Dla was</span>
               </div>
             </Link>
           </div>
-          {!userProfile && (
-          <div className=' px-2 py-4 hidden xl:block'>
-            <div className=' text-gray-400'>
-              Log in to like and comment on Videos
-            </div>
-            <div className='pr-4'>
-              <GoogleLogin 
-              clientId=''
-              render={(renderProps) => (
-                <button 
-                className=' bg-white text-lg text-[#F51997] border-[1px] border-[#F51997] hover:text-white hover:bg-[#F51997] cursor-pointer font-semibold px-6 py-3 rounded-md outline-none w-full mt-3'
-                onClick={renderProps.onClick}
-                disabled={renderProps.disabled}
-                >
-                  Log in
-                </button>
-              )}
-              onSuccess={() => {}}
-              onFailure={() => {}}
-              cookiePolicy='single_host_origin'
-              />
-            </div>
-          </div>
-          )}
+         
 
           <Discover />
           <SuggestedAccounts />
@@ -65,7 +42,7 @@ const normalLink = ' flex items-center gap-3 hover:bg-primary p-3 justify-center
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -6,8 +6,16 @@ import { allPostsQuery } from '../../../utils/queries'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
-    const query = allPostsQuery()
-    const data = await client.fetch(query)
+    const query = allPostsQuery();
+
+    const data = await client.fetch(query);
+
     res.status(200).json(data);
+  } else if (req.method === 'POST') {
+    const doc = req.body;
+
+    client.create(doc).then(() => {
+      res.status(201).json('video created');
+    });
   }
 }
